617851998ec3d58c6f184cede98941e0d04ce290
黄海 <hai.huang.a@outlook.com>
2022-05-25 10:53:48 +0800

    Update

--- src/serializable/Test.java
+++ src/serializable/Test.java
@@ -8,44 +8,41 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
 public class Test {
-	public static void main(String[] args) {
-//		Test.writeSerializableObject();
-		Test.readSerializableObject();
-	}
 
-	public static void writeSerializableObject() {
-		try {
-			Man man = new Man("Huang, Hai", "123456");
-			Person person = new Person(man, "Male", 23);
-			FileOutputStream fileOutputStream = new FileOutputStream("output.txt");
-			ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
-			objectOutputStream.writeObject("This is a Test! ");
-			objectOutputStream.writeObject(person);
-			objectOutputStream.close();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+    @org.junit.Test
+    public void writeSerializableObject() {
+        try {
+            Man man = new Man("Huang, Hai", "123456");
+            Person person = new Person(man, "Male", 23);
+            ObjectOutputStream objectOutputStream;
+            try (FileOutputStream fileOutputStream = new FileOutputStream("output.txt")) {
+                objectOutputStream = new ObjectOutputStream(fileOutputStream);
+                objectOutputStream.writeObject("This is a Test! ");
+                objectOutputStream.writeObject(person);
+            }
+            objectOutputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-	public static void readSerializableObject() {
-		try {
-			FileInputStream fileInputStream = new FileInputStream("output.txt");
-			ObjectInputStream objectInputStream = new ObjectInputStream(
-					fileInputStream/* new FileInputStream("output.txt") */);
-			String string = (String) objectInputStream.readObject();
-			Person person = (Person) objectInputStream.readObject();
-			objectInputStream.close();
-			System.out.println(string);
-			System.out.println("Man's username: " + person.getMan().getUsername());
-			System.out.println("Man's password: " + person.getMan().getPassword());
-			System.out.println("Gender: " + person.getGender());
-			System.out.println("Age: " + person.getAge());
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+    @org.junit.Test
+    public void readSerializableObject() {
+        try {
+            ObjectInputStream objectInputStream;
+            try (FileInputStream fileInputStream = new FileInputStream("output.txt")) {
+                objectInputStream = new ObjectInputStream(fileInputStream);
+            }
+            String string = (String)objectInputStream.readObject();
+            Person person = (Person)objectInputStream.readObject();
+            objectInputStream.close();
+            System.out.println(string);
+            System.out.println("Man's username: " + person.getMan().getUsername());
+            System.out.println("Man's password: " + person.getMan().getPassword());
+            System.out.println("Gender: " + person.getGender());
+            System.out.println("Age: " + person.getAge());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
