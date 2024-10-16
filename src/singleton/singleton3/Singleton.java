6880d06c1775d2fbbfced5c9fc1492f88a176776
海子 <hh01769356@cainiao.com>
2024-10-16 17:06:18 +0800

    Update

--- src/singleton/singleton3/Singleton.java
+++ src/singleton/singleton3/Singleton.java
@@ -9,12 +9,12 @@ package singleton.singleton3;
  */
 public class Singleton {
 
-    private static final Singleton instance = new Singleton();
+    private static final Singleton INSTANCE = new Singleton();
 
     private Singleton() {}
 
     public static Singleton getInstance() {
-        return instance;
+        return INSTANCE;
     }
 
 }
--- src/singleton/singleton4/Singleton.java
+++ src/singleton/singleton4/Singleton.java
@@ -2,19 +2,21 @@ package singleton.singleton4;
 
 /**
  * 第四种（饿汉，变种）
+ * <p>
+ * 表面上看起来差别挺大，其实更第三种方式差不多，都是在类初始化即实例化instance。
  */
 public class Singleton {
 
-    private static Singleton instance = null;
+    private static final Singleton INSTANCE;
 
     static {
-        instance = new Singleton();
+        INSTANCE = new Singleton();
     }
 
     private Singleton() {}
 
     public static Singleton getInstance() {
-        return instance;
+        return INSTANCE;
     }
 
 }
