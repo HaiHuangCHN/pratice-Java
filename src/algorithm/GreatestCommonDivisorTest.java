dceba509e053b938f7dc09e442de3304ebf42e7d
海子 <hh01769356@cainiao.com>
2025-01-10 14:02:29 +0800

    Update

--- src/algorithm/GreatestCommonDivisorTest.java
+++ src/algorithm/GreatestCommonDivisorTest.java
@@ -8,8 +8,8 @@ public class GreatestCommonDivisorTest {
     public void testReverseSequence() {
 //        System.out.println(1112 % 695);
 //        System.out.println(695 % 1112);
-        int result1 = GreatestCommonDivisor.gcd(1112, 695);
-        System.out.println(result1);
+//        int result1 = GreatestCommonDivisor.gcd(1112, 695);
+//        System.out.println(result1);
 //        int result2 = GreatestCommonDivisor.gcd(695, 1112);
 //        System.out.println(result2);
     }
