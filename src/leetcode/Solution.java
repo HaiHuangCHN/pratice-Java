c67648955be1136318428c8db8e50cc58fb34aff
黄海 <hh474440@cainiao.com>
2025-02-27 18:39:54 +0800

    leetcode

---
+++ src/leetcode/Solution.java
@@ -0,0 +1,22 @@
+package leetcode;
+
+/**
+ * https://leetcode.cn/problems/container-with-most-water/
+ */
+class Solution {
+    public int maxArea(int[] height) {
+        // i 和 j 分别是左下标和右下标
+        int i = 0;
+        int j = height.length - 1;
+        int res = 0;
+        while (i < j) {
+            // 以更矮的那边计算面积
+            // 备注：i++，以 i 的旧值计算，+1 后才返回
+            // 之后，矮的那一边向右移（i），或者向左移（j）
+            res = height[i] < height[j] ?
+                    Math.max(res, (j - i) * height[i++]) :
+                    Math.max(res, (j - i) * height[j--]);
+        }
+        return res;
+    }
+}
