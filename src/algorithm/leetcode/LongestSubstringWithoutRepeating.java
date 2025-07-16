9dac115301e19dc20741cde50e3b7136baed769d
黄海 <hh474440@cainiao.com>
2025-07-16 23:14:07 +0800

    Update

--- src/algorithm/leetcode/LongestSubstringWithoutRepeating.java
+++ src/algorithm/leetcode/LongestSubstringWithoutRepeating.java
@@ -6,6 +6,7 @@ package algorithm.leetcode;
  * @author hai.huang.a@outlook.com
  * @date 2025/7/9 00:07
  */
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -20,16 +21,27 @@ public class LongestSubstringWithoutRepeating {
 
 class Solution {
     public int lengthOfLongestSubstring(String s) {
+        // 记录最长无重复子串的长度
         int maxLength = 0;
+        // 滑动窗口的左边界
         int left = 0;
+        // 用于存储字符及其最后一次出现的位置
         Map<Character, Integer> charMap = new HashMap<>();
 
+        // 遍历字符串，right 为滑动窗口的右边界
         for (int right = 0; right < s.length(); right++) {
             char currentChar = s.charAt(right);
+
+            // 如果当前字符已经在 Map 中，并且其最后一次出现的位置在左边界右侧
+            // 则需要将左边界移动到该字符最后一次出现位置的下一个位置
             if (charMap.containsKey(currentChar) && charMap.get(currentChar) >= left) {
                 left = charMap.get(currentChar) + 1;
             }
+
+            // 更新当前字符的最后出现位置
             charMap.put(currentChar, right);
+
+            // 计算当前窗口的长度，并更新最大长度
             maxLength = Math.max(maxLength, right - left + 1);
         }
 
